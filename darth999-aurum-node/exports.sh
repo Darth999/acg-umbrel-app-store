dd6f3e93251b52f3b73fc807c700b59a4540e8f2
Darth999 <rolf.wer@googlemail.com>
2026-04-11 23:12:28 +0200

    Update exports.sh

--- darth999-aurum-node/exports.sh
+++ darth999-aurum-node/exports.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-APP_AURUM_NODE_IP="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' darth999-aurum-node_aurum-node_1 2>/dev/null)"
+SOURCE_DIR="/home/umbrel/umbrel/app-stores/darth999-acg-umbrel-app-store-github-79e618ae/darth999-aurum-node"
+TARGET_DIR="/home/umbrel/umbrel/app-data/darth999-aurum-node"
+
+mkdir -p "${TARGET_DIR}"
+cp -f "${SOURCE_DIR}/nginx.conf" "${TARGET_DIR}/nginx.conf"
+cp -f "${SOURCE_DIR}/index.html" "${TARGET_DIR}/index.html"
